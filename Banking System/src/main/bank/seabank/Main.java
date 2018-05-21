418115b9a728c1feb0f71fdc2b0ef329dff6236e
John Anselmo <jjohnanselmo@gmail.com>
2018-05-21 11:38:43 -0400

    Login mismatchexception fifix

--- Banking System/src/main/bank/seabank/Main.java
+++ Banking System/src/main/bank/seabank/Main.java
@@ -3,14 +3,17 @@ package main.bank.seabank;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.NumberFormat;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
+import static java.lang.Integer.parseInt;
+
 /*
     TODO
     1) Try catches for inputmismatchexceptions
-    2) Fix print statement as input for user input
+    2) Add in b for back
  */
 
 
@@ -23,34 +26,57 @@ public class Main {
         } catch(InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-        System.out.println("Please select an item below:");
-        System.out.println("(0) Login");
-        System.out.println("(1) Create Account");
         mainMenu();
     }
 
     public static void mainMenu() {
+        System.out.println("Please select an item below:");
+        System.out.println("(0) Login");
+        System.out.println("(1) Create Account");
         System.out.print(">> ");
         try {
-            int mainMenu = s.nextInt();
+            int mainMenu = parseInt(s.next());
             if (mainMenu == 0) {
                 login();
             } else if (mainMenu == 1) {
                 System.out.println("coo create acc"); //add in account creation
             }
-        } catch(InputMismatchException e) {
-            System.out.println("\nPlease enter an integer");
+        } catch(NumberFormatException e) {
+            System.out.println("\nPlease enter an integer\n");
             mainMenu();
         }
     }
 
+
+    /*
+        START OF TREE FOR login OPTION
+     */
     public static void login() {
         System.out.print("Account number: ");
-        int login_accNum = s.nextInt();
-        if(new File(System.getProperty("user.dir") + "/data/accounts/" + login_accNum + ".txt").exists()) {
-            System.out.println("Account #" + login_accNum + " found!\n");
-            promptForPass(login_accNum);
+        try {
+            int login_accNum = parseInt(s.next());
+            if (new File(System.getProperty("user.dir") + "/data/accounts/" + login_accNum + ".txt").exists()) {
+                System.out.println("Account #" + login_accNum + " found!\n");
+                promptForPass(login_accNum);
+            }
+            else {
+                System.out.println("\nAccount #" + login_accNum + " does not seem to exist.\n"  +
+                                    "Would you like to create an account?");
+                System.out.print("(yY/nN)>> ");
+                String createAcc_failedLogin = s.next();
+                if(createAcc_failedLogin.toLowerCase().equals("y")) {
+
+                }
+                else if(createAcc_failedLogin.toLowerCase().equals("y")) {
+                    
+                }
+
+            }
+        } catch(NumberFormatException e) {
+            System.out.println("\nPlease enter an integer\n");
+            login();
         }
+
     }
 
     public static void promptForPass(int accToAccess) {
@@ -62,30 +88,42 @@ public class Main {
         }
         else {
 //            cls();
-            System.out.println("Incorrect password");
+            System.out.println("\nIncorrect password");
             promptForPass(accToAccess);
         }
     }
 
     public static void display(int accountToDisplay) {
         System.out.println("Account #" + accountToDisplay);
-        System.out.println("\tBalance: $" + Account.getBalance(accountToDisplay));
+        System.out.println("\n\tBalance: $" + Account.getBalance(accountToDisplay));
         System.out.println("\n\t(0) Withdraw Money");
         System.out.println("\t(1) Deposit Money");
         System.out.println("\t(2) Log a Transaction");
 
-        int accountOption = s.nextInt();
-        if(accountOption == 0) {
+        try {
+            int accountOption = parseInt(s.next());
+            if (accountOption == 0) {
 
-        }
-        else if(accountOption == 1) {
+            } else if (accountOption == 1) {
 
-        }
-        else if(accountOption == 2) {
+            } else if (accountOption == 2) {
 
+            }
+        } catch(NumberFormatException e) {
+            System.out.println("\nPlease enter an integer\n");
         }
 
     }
+    /*
+        END OF TREE FOR login OPTION
+     */
+
+
+    /*
+        START OF TREE FOR create account OPTION
+     */
+
+
 
 //    public static void cls() {
 //        if(System.getProperty("os.name").toLowerCase().indexOf("win") > -1) {
